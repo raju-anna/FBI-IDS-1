4660539b070460975a28efe2f20a4d1cb5e8811b
Dhruv <dhruvmanand0729@gmail.com>
2026-01-08 20:40:50 +0000

    Refactor worker logic

--- include/Worker.hpp
+++ include/Worker.hpp
@@ -2,17 +2,18 @@
 #include "Queue.hpp"
 #include "Packet_Parser.hpp"
 #include <atomic>
-#include "Rules.hpp"
+#include "FlowTable.hpp"
+
 
 class Worker {
 public:
-    Worker(ThreadSafeQueue &q, int id, std::atomic<bool> &running_flag, const std::vector<Rule>& rules);
+    Worker(ThreadSafeQueue &q, int id, std::atomic<bool> &running_flag, FlowTable &flow_table);
     void operator()();
 
 private:
     ThreadSafeQueue &queue_;
     int id_;
     std::atomic<bool> &running_;
+    FlowTable &flow_table_;
 
-    const std::vector<Rule>& rules_;
 };
--- src/main2.cpp
+++ src/main2.cpp
@@ -7,28 +7,30 @@
 #include "Capture.hpp"
 #include "Queue.hpp"
 #include "Worker.hpp"
-#include "Rules.hpp"
-#include "Rule_Parser.hpp"
 
 static CaptureEngine *g_cap = nullptr;
 static std::atomic<bool> g_running{true};
 
-void handle_sigint(int) {
+void handle_sigint(int)
+{
     g_running.store(false);
-    if (g_cap) g_cap->break_loop();
+    if (g_cap)
+        g_cap->break_loop();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     std::string dev;
-    if (argc > 1) dev = argv[1];
-    else {
+    if (argc > 1)
+        dev = argv[1];
+    else
+    {
         std::cerr << "Usage: sudo ./ids <interface>\n";
         return 1;
     }
 
-    RuleParser parser;
-    auto rules = parser.parse_file("config/local.txt");
-    std::cout << "[+] Loaded " << rules.size() << " rules\n";
+    uint64_t idle_timeout_us = 30 * 1000 * 1000; // 30 seconds
+    FlowTable flow_table(idle_timeout_us);
 
     // create queue
     ThreadSafeQueue queue;
@@ -39,8 +41,9 @@ int main(int argc, char* argv[]) {
     // start workers
     unsigned int num_workers = std::max(1u, std::thread::hardware_concurrency() / 2);
     std::vector<std::thread> workers;
-    for (unsigned int i = 0; i < num_workers; ++i) {
-        Worker w(queue, static_cast<int>(i), g_running, rules);
+    for (unsigned int i = 0; i < num_workers; ++i)
+    {
+        Worker w(queue, static_cast<int>(i), g_running, flow_table);
         workers.emplace_back(std::thread(std::move(w)));
     }
 
@@ -48,27 +51,37 @@ int main(int argc, char* argv[]) {
     CaptureEngine cap;
     g_cap = &cap;
     std::string err;
-    if (!cap.open_live(dev, 65535, true, 1000, &err)) {
+    if (!cap.open_live(dev, 65535, true, 1000, &err))
+    {
         std::cerr << "pcap_open_live failed: " << err << "\n";
         // signal workers to stop and join
         queue.close();
-        for (auto &t : workers) if (t.joinable()) t.join();
+        for (auto &t : workers)
+            if (t.joinable())
+                t.join();
         return 1;
     }
 
     std::cout << "[+] capture started on " << dev << " workers=" << num_workers << "\n";
     int ret = cap.loop(0, &queue); // block until break
-    if (ret == -1) {
+    if (ret == -1)
+    {
         std::cerr << "pcap_loop error: " << pcap_geterr(nullptr) << "\n";
-    } else if (ret == -2) {
+    }
+    else if (ret == -2)
+    {
         std::cout << "[*] pcap_loop terminated via break\n";
-    } else {
+    }
+    else
+    {
         std::cout << "[*] pcap_loop exited: " << ret << "\n";
     }
 
     // cleanup
     queue.close();
-    for (auto &t : workers) if (t.joinable()) t.join();
+    for (auto &t : workers)
+        if (t.joinable())
+            t.join();
     cap.close();
     g_cap = nullptr;
     std::cout << "[+] shutdown complete\n";
